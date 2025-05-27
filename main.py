e5eaf6480d1dbe45e4294c4798368bc7de1f46cd
haco8534 <siduki0107@yahoo.co.jp>
2025-05-27 23:44:46 +0900

    changed directory structuer

--- main.py
+++
@@ -1,9 +0,0 @@
-from modules.scenario import ScenarioService
-from modules.image import ImageSetService
-from pprint import pprint
-
-
-theme = "【ORMの定番】SQLAlchemyを分かりやすく解説！PythonからDBを簡単操作〜初心者向け〜"
-minutes = 3
-
-scenario = ScenarioService()
